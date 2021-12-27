c384c594369eb244e03379ed7a823f491b4bee75
shanksms <sriv.srivastava@gmail.com>
2021-12-28 00:40:01 +0530

    save user id and password in db

---
+++ store_password_in_db/item.py
@@ -0,0 +1,51 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from flask import request
+
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        filtered_items = [item for item in items if item['name'] == name]
+        if len(filtered_items) == 0:
+            return {'message': 'item not found'}, 404
+        else:
+            return filtered_items[0], 200
+
+    def post(self, name):
+        request_data = request.get_json()
+        existing_items = [item for item in items if name == item['name']]
+        if len(existing_items) != 0:
+            return {'message': 'item already exists'}, 400
+        item = {'name': name, 'price': request_data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = [item for item in items if item['name'] != name]
+        return {'message': 'item deleted'}
+
+    def put(self, name):
+        request_parser = reqparse.RequestParser()
+        request_parser.add_argument(
+            'price',
+            type=float,
+            required=True,
+            help='This field can not be left blank'
+        )
+        #data = request.get_json()
+        data = request_parser.parse_args()
+        filtered_items = [item for item in items if item['name'] == name]
+        if len(filtered_items) == 0:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item = filtered_items[0]
+            item.update(data)
+        return item
+
+class ItemList(Resource):
+
+    def get(self):
+        return {'items': items}, 200
--- store_password_in_db/user.py
+++ store_password_in_db/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 
 class User:
@@ -33,6 +34,41 @@ class User:
 
     def __str__(self):
         return f'username: {self.username}'
+
+
+class UserRegister(Resource):
+    request_parser = reqparse.RequestParser()
+    request_parser.add_argument(
+        'username',
+        type=str,
+        required=True,
+        help='This field can not be left blank'
+    )
+    request_parser.add_argument(
+        'password',
+        type=str,
+        required=True,
+        help='This field can not be left blank'
+    )
+
+    def post(self):
+        data = UserRegister.request_parser.parse_args()
+        if User.get_user_by_name(data['username']):
+            return {'message': 'user already exists'}, 400
+        try:
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+            insert_query = '''
+                   insert into user
+                   (username, password)
+                   values
+                   (?, ?)
+                   '''
+            cursor.execute(insert_query, (data['username'], data['password']))
+            connection.commit()
+        finally:
+            connection.close()
+        return {'message': 'successfully created the user'}
 
 
 if __name__ == '__main__':
