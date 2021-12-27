b172c209e2e5f07f0b00c246e134f4eaa1b70236
shanksms <sriv.srivastava@gmail.com>
2021-12-27 10:19:52 +0530

    using restful module

--- app_flask_restful.py
+++ app_flask_restful.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
 api = Api(app)
+items = []
 
-
-class Student(Resource):
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'name': None}, 404
 
 
-api.add_resource(Student, '/student/<string:name>')
-app.run(port=5000)
+    def get(self):
+        return {'items': items}
+
+
+    def post(self):
+        request_data = request.get_json()
+        item = {'name': request_data['name'], 'price': request_data['price']}
+        items.append(item)
+        return item, 201
+
+
+api.add_resource(Item, '/items/<string:name>', '/items')
+
+app.run(port=5000, debug=True)
